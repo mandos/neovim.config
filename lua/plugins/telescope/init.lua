7fab6bcd4dbba1059439c12a57d630ffa62a6e5d
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2024-12-05 11:10:42 +0900

    fix: Telescope configuration

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -2,11 +2,27 @@ vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>hh", "<cmd>Telescope help_tags<cr>")
 vim.keymap.set("n", "<leader>hk", "<cmd>Telescope keymaps<cr>")
 vim.keymap.set("n", "<leader>st", "<cmd>Telescope live_grep<cr>")
-
 return {
 	"nvim-telescope/telescope.nvim",
 	cmd = "Telescope",
-	opts = require("plugins.telescope.options"),
+	config = function()
+		require("telescope").setup({
+
+			defaults = {
+
+				mappings = {
+					i = {
+						["<esc>"] = require("telescope.actions").close,
+					},
+				},
+				sorting_strategy = "ascending",
+				layout_config = {
+					prompt_position = "top",
+					vertical = { mirror = true },
+				},
+			},
+		})
+	end,
 	dependencies = {
 		{ "nvim-lua/popup.nvim" },
 		{ "nvim-lua/plenary.nvim" },
--- lua/plugins/telescope/options.lua
+++ lua/plugins/telescope/options.lua
@@ -1,15 +1,1 @@
-return {
-	defaults = {
-
-		mappings = {
-			i = {
-				["<esc>"] = require("telescope.actions").close,
-			},
-		},
-		sorting_strategy = "ascending",
-		layout_config = {
-			prompt_position = "top",
-			vertical = { mirror = true },
-		},
-	},
-}
+return {}
