4b2d4025bd2c7fceeb296d84dd0a57c9a72120b3
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2025-02-10 11:27:32 +0900

    plug: Add Markdown Preview plugin

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,3 +1,6 @@
+vim.api.nvim_set_keymap("n", "<leader>tm", "<cmd>MarkdownPreviewToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>om", "<cmd>MarkdownPreview<CR>", { noremap = true, silent = true })
+
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
